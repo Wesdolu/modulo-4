7a0b97bfeee80fde4853a0b45ca12056d2d87be2
Wesdolu <wesley04052011@gmail.com>
2026-01-06 15:00:30 +0000

    Add files via upload

---
+++ NoveEmeia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int rui, mae;
+	int contador = 1;
+	int resposta = 0;
+	cin >> rui;
+	cin >> mae;
+	do
+	{contador++;
+		rui++;
+		mae++;
+
+		
+			
+		
+	} while (rui != mae/2);
+	cout << contador;
+}
